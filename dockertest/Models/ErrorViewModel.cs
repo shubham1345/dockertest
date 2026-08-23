c3fcb3e155a750120f472fe63055e3c65320d268
shubham choudhary <shubhamchoudhary1345@gmail.com>
2026-08-23 18:22:17 +0530

    Add project files.

---
+++ dockertest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dockertest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
